6dfa3c0061a5701c940014876101415143413f86
roma.sck <roma.sck@gmail.com>
2016-03-17 13:34:04 +0200

    all required functionality works

--- app/src/main/java/com/example/sck/androidintership_task1/MainActivity.java
+++ app/src/main/java/com/example/sck/androidintership_task1/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.sck.androidintership_task1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Звернення № СЕ-1257218");
 
         initToolbar();
+
+        initRecyclerView();
+
+        ViewGroup allContent = (ScrollView) findViewById(R.id.content);
+        setOnClickAllViews(allContent);
     }
 
     private void initToolbar() {
@@ -34,5 +46,41 @@ public class MainActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    private void initRecyclerView() {
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        assert recyclerView != null;
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, getResources().getStringArray(R.array.image_urls));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setOnClickAllViews(ViewGroup viewGroup) {
+
+        int childCount = viewGroup.getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            View view = viewGroup.getChildAt(i);
+            // if ViewGroup - finding inside
+            if (view instanceof ViewGroup) {
+                setOnClickAllViews((ViewGroup) view);
+            } else if (view instanceof TextView || view instanceof ImageView) {
+                // if view - set onClick
+                view.setOnClickListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // shows simple name of clicked view
+        Toast.makeText(this, view.getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
     }
 }
