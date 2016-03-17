1ff37d8ed84522cae2100aaacf78f3affb36f1fe
roma.sck <roma.sck@gmail.com>
2016-03-17 12:43:55 +0200

    added main functional without recycler view,add images to assets

--- app/src/main/java/com/example/sck/androidintership_task1/MainActivity.java
+++ app/src/main/java/com/example/sck/androidintership_task1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sck.androidintership_task1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Звернення № СЕ-1257218");
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        assert toolbar != null;
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
